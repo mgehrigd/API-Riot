2996b621cdb17712800862fae537c3f782543950
M Gehrig <46573897+mgehrigd@users.noreply.github.com>
2019-01-10 20:18:22 +0100

    Update RiotApiTest.java

--- test/java/net/rithms/test/RiotApiTest.java
+++ test/java/net/rithms/test/RiotApiTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 Taylor Caldwell
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package net.rithms.test;
 
@@ -52,12 +37,7 @@ public class RiotApiTest {
 		return new RiotApi(config);
 	}
 
-	/**
-	 * This method provides an {@link RiotApiAsync} object with the {@code apiKey} specified in this class to other tests.
-	 * 
-	 * @return {@code RiotApiAsync} object
-	 * @see RiotApiAsync
-	 */
+
 	public static RiotApiAsync getRiotApiAsync() {
 		return getRiotApi().getAsyncApi();
 	}
